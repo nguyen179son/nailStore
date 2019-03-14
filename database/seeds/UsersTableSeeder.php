591de89789b69acf0220b6e468cc4f7e34ba1b50
minhpham <minhpham@apples-MacBook-Pro.local>
2019-03-14 21:40:19 +0100

    fix seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,8 +12,8 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'chutruyen',
-            'email' => 'chutruyendeptrai@gmail.com',
+            'user_name' => 'chutruyen',
+//            'email' => 'chutruyendeptrai@gmail.com',
             'password' => bcrypt('truyendeptraihihi'),
         ]);
     }
