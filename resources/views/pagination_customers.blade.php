e08b500f35fee875d43623f9a12a760930f22327
minhpham <minhpham@student-208-145.eduroam.uu.se>
2019-03-15 17:15:23 +0100

    customer management screens and modals

---
+++ resources/views/pagination_customers.blade.php
@@ -0,0 +1,26 @@
+<table class="table table-striped table-hover">
+    <thead>
+    <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Times</th>
+    </tr>
+    </thead>
+    <tbody>
+    @if (isset($members) && !empty($members))
+        @foreach ($members as $key => $member)
+            <tr>
+                <td>{{ ($members->currentPage()-1)*10+$key+1 }}</td>
+                <td><a href="#" data-toggle="modal" data-target="#history-modal" data-id="{{$member->id}}" class="member-name">{{$member->name}}</a></td>
+                <td>{{$member->email}}
+                </td>
+                <td>{{$member->point}}</td>
+            </tr>
+        @endforeach
+    @endif
+
+    </tbody>
+</table>
+
+{!! $members->links() !!}
