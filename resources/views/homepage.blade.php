590e7d010e7cfcb84193fc66df338881c2a91872
hung <dshung1997@gmail.com>
2019-04-06 19:45:49 +0200

    Add discount modal

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -279,6 +279,31 @@
 </div>
 <!-- <hr class="my-4 hr-style-1"> -->
 
+
+<!-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#discount-modal">
+  Launch demo modal
+</button> -->
+<div class="modal fade" id="discount-modal" tabindex="-1" role="dialog" aria-labelledby="discount-modal" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content discount-modal-content">
+      <div class="modal-header discount-modal-header">
+        <!-- <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5> -->
+        <button type="button" class="close discount-modal-close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <img alt="picture" src="{{ URL::asset('/images/discount_image_21_04_19.png') }}"
+                     class="img-fluid">
+      </div>
+      <!-- <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div> -->
+    </div>
+  </div>
+</div>
+
 <footer class="page-footer font-small">
 
     <!-- Copyright -->
